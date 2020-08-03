3fb0127c03eec273d9a1e94195c0e6a821a73a26
xiehui <xiehui@yuewen.com>
2020-08-03 16:46:52 +0800

    更新资源sql

--- src/main/resources/script/db_entity.sql
+++ src/main/resources/script/db_entity.sql
@@ -1,11 +1,15 @@
-CREATE TABLE `easy_pool_demo`.`db_entity` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `driver_class_name` VARCHAR(45) NULL DEFAULT 'com.mysql.cj.jdbc.Driver' COMMENT '驱动类',
-  `jdbc_url` VARCHAR(45) NULL COMMENT '数据库连接地址,必填！',
-  `pool_name` VARCHAR(45) NULL COMMENT '连接池名称,必填！',
-  `username` VARCHAR(45) NULL COMMENT '用户名',
-  `password` VARCHAR(45) NULL COMMENT '密码',
-  `minimum_idle` INT NULL DEFAULT 5 COMMENT '最小空闲连接数',
-  `maximum_pool_size` INT NULL DEFAULT 10 COMMENT '最大连接数',
-  `connection_test_query` VARCHAR(45) NULL DEFAULT 'SELECT 1' COMMENT '测试连接是否有效SQL',
-  PRIMARY KEY (`id`));
+CREATE TABLE `db_entity` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `driver_class_name` varchar(45) DEFAULT 'com.mysql.cj.jdbc.Driver' COMMENT '驱动类',
+  `jdbc_url` varchar(45) DEFAULT NULL COMMENT '数据库连接地址<jdbc:mysql://ip:port/db>,必填！',
+  `pool_name` varchar(45) NOT NULL COMMENT '连接池名称,必填！',
+  `username` varchar(45) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(45) DEFAULT NULL COMMENT '密码',
+  `minimum_idle` int(11) DEFAULT '5' COMMENT '最小空闲连接数',
+  `maximum_pool_size` int(11) DEFAULT '10' COMMENT '最大连接数',
+  `connection_test_query` varchar(45) DEFAULT 'SELECT 1' COMMENT '测试连接是否有效SQL',
+  `group_name` varchar(45) DEFAULT NULL COMMENT '分组名',
+  `balance_type` varchar(45) DEFAULT NULL COMMENT '负载均衡类型',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `pool_name_UNIQUE` (`pool_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
